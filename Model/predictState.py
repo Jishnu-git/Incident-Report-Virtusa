ef423397dd7394b01a8756814c6753d7b64f9098
Jishnu <jishnu30alamelu26@gmail.com>
2021-08-05 23:42:03 +0530

    Updated the model to perform and use simple forecasts for predictions

--- Model/predictState.py
+++ Model/predictState.py
@@ -1,15 +1,41 @@
 import pandas as pd
-from Regression import regressionModel, labeler
+from Regression.regressionModel import RegressionModel
+from AutoRegression import autoRegressionModel
+import labeler
 import os
 import json
 
-def regression(trainData : "DataFrame", testData : "DataFrame") -> tuple:
-    output = regressionModel.makePrediction(trainData, testData, "Failure Rate")
-    predictedData = testData.drop("Failure Rate", axis=1)
-    predictedData["Failure Rate"] = output["Prediction"]
-    predictedData["Status"] = labeler.label(predictedData)
+def regression(trainData : "DataFrame", testData : "DataFrame", trainingWindow : int) -> tuple:
+    regressionModel = RegressionModel(trainData, "Failure Rate")
+    statePredictions = ["-"] * (trainingWindow - 1)
+    failurePredictions = ["-"] * (trainingWindow - 1)
+    for i in range(0, testData.shape[0] - trainingWindow):
+        currentWindow = testData.iloc[i:(i + trainingWindow)]
+        forecastUsers = autoRegressionModel.makePrediction(currentWindow, "Requests", 5)
+        forecastResTime = autoRegressionModel.makePrediction(currentWindow, "Response Time", 5)
+        forecastWindow = pd.DataFrame({"Response Time": forecastResTime, "Requests": forecastUsers})
 
-    return output["Attributes"], predictedData
+        forecastAggRaw = forecastWindow.agg(["mean", "min", "max"])
+        forecastAggFormatted = pd.DataFrame({
+            "Average Response Time": [forecastAggRaw["Response Time"]["mean"]],
+            "Minimum Response Time": [forecastAggRaw["Response Time"]["min"]],
+            "Maximum Response Time": [forecastAggRaw["Response Time"]["max"]],
+            "Average Request Count": [forecastAggRaw["Requests"]["mean"]],
+            "Minimum Request Count": [forecastAggRaw["Requests"]["min"]],
+            "Maximum Request Count": [forecastAggRaw["Requests"]["max"]],
+        })
+        failurePredictions.append(regressionModel.makePrediction(forecastAggFormatted)[0])
+        forecastAggFormatted["Failure Rate"] = [failurePredictions[-1]]
+        print(forecastAggFormatted)
+        statePredictions.extend(labeler.label(forecastAggFormatted))
+
+    failurePredictions.append("-")
+    statePredictions.append("-")
+
+    predictedData = testData
+    predictedData["Predicted Failure"] = failurePredictions
+    predictedData["Status"] = statePredictions
+    return regressionModel.getAttributes(), predictedData
 
 if __name__ == "__main__":
     configJSON = open("./config.json", "r")
@@ -25,13 +51,14 @@ if __name__ == "__main__":
     predictionFile = config["predictedOutputFile"]
     modelFile = config["modelOutputFile"]
     method = config["model"]
+    trainingWindow = config["trainingWindow"]
 
     for outputFile in [predictionFile, modelFile]:
         outputDir = os.path.dirname(outputFile)
         if not os.path.exists(outputDir):
             os.makedirs(outputDir)
 
-    modelAttributes, predictedData = models[method](trainData, testData)
+    modelAttributes, predictedData = models[method](trainData, testData, trainingWindow)
     predictedData.to_csv(predictionFile, index=False)
     with open(modelFile, "w") as modelOutput:
         json.dump(modelAttributes, modelOutput)
